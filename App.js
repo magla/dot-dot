33dc2557c0f5b7b42529031ca121fd113df24eaf
Magdalena Magličić <magdalena.maglicic@fiveminutes.eu>
2018-10-22 00:45:08 +0200

    Package.json solution with eslint and jsconfig.js

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import WelcomeScreen from './modules/landing/screens/Welcome';
+import WelcomeScreen from '~/landing/screens/Welcome';
 
 export default function App() {
   return <WelcomeScreen />;
